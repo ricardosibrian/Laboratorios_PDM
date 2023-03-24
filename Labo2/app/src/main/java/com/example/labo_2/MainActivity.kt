a65a620913f78dd33ee786ee24035f2ff02f83ba
ricardosibrian <00173821@uca.edu.sv>
2023-03-24 16:06:41 -0600

    Labo2

---
+++ Labo2/app/src/main/java/com/example/labo_2/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.labo_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var weight: EditText
+    private lateinit var height: EditText
+    private lateinit var ButtonCalculate: Button
+    private lateinit var TextViewBMI: TextView
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        }
+    }
